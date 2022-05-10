2fdf076fe0b733cdc68cf7fd63e4623a28ea87ce
User0092 <103274860+User00092@users.noreply.github.com>
2022-05-10 19:34:44 -0400

    Create anti_admin.lua

---
+++ GTA/anti_admin.lua
@@ -0,0 +1,22 @@
+local admin, player_index
+function OnInit()
+    console.log("Loaded anti-admin")
+    admin = not admin
+end
+
+function OnPlayerJoin(ply)
+	player_index = player.index()
+	if (player.is_rockstar_dev(ply) == true) then
+		admin = true
+	end
+
+    if (admin) == true then
+        utils.notify("Anti-admin","Admin detected", 17, 4)
+        lobby.change_session(0)
+        admin = not admin
+    end
+end
+
+function OnDone()
+    console.log("Unloaded anti-admin")
+end
