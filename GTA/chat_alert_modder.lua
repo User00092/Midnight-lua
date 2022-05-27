6508bc98f80c2684237d57259d99326f8bdd31d9
User0092 <103274860+User00092@users.noreply.github.com>
2022-05-27 10:34:59 -0400

    Update chat_alert_modder.lua

--- GTA/chat_alert_modder.lua
+++ GTA/chat_alert_modder.lua
@@ -1,26 +1,34 @@
 -- made by User0092
-
-function OnInit()
-    console.log("\nLoaded\n")
+local function Log(text)
+	console.log(con_color.Blue, "[Alert_Modder] ")
+	console.log(con_color.White, text .. "\n")
 end
-
-local username, alert_reason
+function OnInit()
+    Log("Loaded")
+end
 
 
 function OnModderDetected(ply, reason)
-    username = player.get_name(ply)
+	local alert_reason
+    local username = player.get_name(ply)
 	if reason == 64 then
 		alert_reason = "Super Jump"
 	elseif reason == 8192 then
 		alert_reason = "Kick Attempt"
 	elseif reason == 512 then
 		alert_reason = "Censor Bypass"
+	elseif reason == 1024 then 
+		alert_reason = "Bad/Invalid Name"
+	elseif reason == 16 then 
+		alert_reason = "CRCs Mismatch"
+	elseif reason == 2 then 
+		alert_reason = "Force Host"
 	else
 		alert_reason = reason .. " .. (Contact developer with code and reason)"
 	end
-    utils.send_chat("" .. username .. " Is cheating. Reason: " .. alert_reason .. "", false, 1)
+    utils.send_chat( username .. " Is cheating. Reason: " .. alert_reason .. "", false, ply)
 end
 
 function OnDone()
-    console.log("\nUnloaded\n")
+    Log("Unloaded")
 end
